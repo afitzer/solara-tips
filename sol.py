94da461d88e812ce7343c35a26d26676b2024f91
afitzer <alexpfitzgerald@yahoo.com>
2023-05-12 21:54:42 -0500

    example code from the webapp tutorial

--- sol.py
+++ sol.py
@@ -1,26 +1,18 @@
 import solara
 
-# Declare reactive variables at the top level. Components using these variables
-# will be re-executed when their values change.
-sentence = solara.reactive("Solara makes our team more productive.")
-word_limit = solara.reactive(10)
+clicks = solara.reactive(0)
 
 
 @solara.component
 def Page():
-    # Calculate word_count within the component to ensure re-execution when reactive variables change.
-    word_count = len(sentence.value.split())
+    color = "green"
+    if clicks.value >= 5:
+        color = "red"
 
-    # Write a title
-    solara.Title("Word count")
+    def increment():
+        clicks.value += 1
+        print("clicks", clicks)
 
-    solara.InputText(label="Your sentence", value=sentence, continuous_update=True)
-    solara.SliderInt("Word limit", value=word_limit, min=2, max=20, thumb_label=True)
+    label = "Not clicked yet" if clicks.value == 0 else f"Clicked: {clicks}"
+    solara.Button(label=label, on_click=increment, color=color)
 
-    # Display messages based on the current word count and word limit.
-    if word_count >= int(word_limit.value):
-        solara.Error(f"With {word_count} words, you passed the word limit of {word_limit.value}.")
-    elif word_count >= int(0.8 * word_limit.value):
-        solara.Warning(f"With {word_count} words, you are close to the word limit of {word_limit.value}.")
-    else:
-        solara.Success("Great short writing!")
